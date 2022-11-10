6fe1f9050b1ef41ede8834c93ffa634e82981fb4
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-11-10 03:03:13 -0800

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,29 @@ void main() {
   ));
 }
 
-class NavigationBar extends StatelessWidget {
+class NavigationBar extends StatefulWidget {
   const NavigationBar({Key? key}) : super(key: key);
 
+  @override
+  State<NavigationBar> createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Text('Text'),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) => setState(() {
+          _currentIndex = value;
+        }),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'HOME'),
           BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Office'),
           BottomNavigationBarItem(icon: Icon(Icons.school), label: 'SCHOOL'),
         ],
-        currentIndex: 0,
+        currentIndex: _currentIndex,
       ),
     );
   }
